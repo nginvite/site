e80d5197ba38ca8490a76bac43ff3289641b0347
Haerul Muttaqin <haerulmuttaqin@gmail.com>
2021-04-24 00:31:20 +0700

    update

--- panel/application/models/Pengguna_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Pengguna_model extends CI_Model
-{
-    public function registrasi($data)
-    {
-        $this->db->insert('tabel_pengguna', $data);
-        // return $this->db->affected_rows();
-    }
-}
